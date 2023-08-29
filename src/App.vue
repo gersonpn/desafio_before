45e48897d58d2057f3eebdb1ced9faf4293b0744
gersonpn <gerson.neto@estudante.ifms.edu.br>
2023-08-29 15:41:08 -0400

    axios para consumo da api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <img src="./assets/vue.svg">
+    <pokemon></pokemon>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue';
+import Pokemon from './components/Pokemon.vue';
+
+ export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Pokemon
+  }
+ }
+
+
+
+
+</script>
+
+
+
+<style>
+
+#app {
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
